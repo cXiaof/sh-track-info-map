aaa4089c542ec1d27f383a4a152d820f9b04fcfd
cXiaof <310257509@qq.com>
2022-03-25 01:23:49 +0800

    json分包

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,28 +1,19 @@
 import React from 'react'
-import { useBoolean, useMemoizedFn, useMount } from 'ahooks'
+import { useMemoizedFn, useMount } from 'ahooks'
 import { Modal } from 'antd-mobile'
-import axios from 'axios'
-import dayjs from 'dayjs'
 
 import Notice from './notice'
 import Legend from '@@/containers/Legend'
 import Zoom from '@@/containers/Zoom'
 import Location from '@@/containers/Location'
 
-const time = dayjs().valueOf()
-
 const App = React.memo(() => {
-    const [riskOver, riskOverActions] = useBoolean(false)
-    const [trackOver, trackOverActions] = useBoolean(false)
-
     const animate2Location = useMemoizedFn(() => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 const { longitude, latitude } = position.coords
-                window.map.animateTo({
-                    center: [longitude, latitude],
-                    zoom: 16,
-                })
+                const center = [longitude, latitude]
+                window.map.animateTo({ center, zoom: 16 })
             },
             (error) => {
                 console.error(error)
@@ -31,34 +22,18 @@ const App = React.memo(() => {
         )
     })
 
-    const renderRisk = useMemoizedFn(async () => {
-        const result = await axios.get(`./data/risk.geojson?_t=${time}`)
-        window.map.getLayer('risk').addGeometry(result.data)
-        riskOverActions.setTrue()
-    })
-
-    const renderTrack = useMemoizedFn(async () => {
-        const result = await axios.get(`./data/track.geojson?_t=${time}`)
-        Object.values(result.data).forEach((track) =>
-            window.map.getLayer('track').addGeometry(track)
-        )
-        trackOverActions.setTrue()
-    })
-
     useMount(() => {
         Modal.clear()
         Modal.alert({
             content: <Notice />,
             onConfirm: animate2Location,
         })
-        renderRisk()
-        renderTrack()
     })
 
     return (
         <div>
             <div className='absolute left-0 bottom-0 pl-5 pb-11 space-y-4'>
-                <Legend riskOver={riskOver} trackOver={trackOver} />
+                <Legend />
             </div>
             <div className='absolute text-primary right-0 bottom-0 pr-5 pb-11 space-y-4'>
                 <Zoom />
--- src/containers/Legend/index.tsx
+++ src/containers/Legend/index.tsx
@@ -1,25 +1,59 @@
-import React, { useMemo } from 'react'
-import { DotLoading } from 'antd-mobile'
+import React from 'react'
 import {
     LocationMarkerIcon,
     ExclamationCircleIcon,
 } from '@heroicons/react/solid'
+import { useBoolean, useMemoizedFn, useMount } from 'ahooks'
+import { DotLoading } from 'antd-mobile'
+import dayjs from 'dayjs'
+import axios from 'axios'
 
-export interface LegendProps {
-    riskOver: boolean
-    trackOver: boolean
-}
+const time = dayjs().valueOf()
 
-const Legend = React.memo(({ riskOver, trackOver }: LegendProps) => {
-    const trackIcon = useMemo(
-        () =>
-            trackOver ? (
-                <ExclamationCircleIcon className='h-4' />
-            ) : (
-                <DotLoading color='currentColor' />
-            ),
-        [trackOver]
-    )
+const Legend = React.memo(() => {
+    const [riskOver, riskOverActions] = useBoolean(false)
+    const [track3, track3Actions] = useBoolean(false)
+    const [track7, track7Actions] = useBoolean(false)
+    const [track14, track14Actions] = useBoolean(false)
+    const [trackLong, trackLongActions] = useBoolean(false)
+
+    const renderRisk = useMemoizedFn(async () => {
+        const result = await axios.get(`./data/risk.geojson?_t=${time}`)
+        window.map.getLayer('risk').addGeometry(result.data)
+        riskOverActions.setTrue()
+    })
+
+    const renderTrack3 = useMemoizedFn(async () => {
+        const result = await axios.get(`./data/track_3.geojson?_t=${time}`)
+        renderTrackGeo(result)
+        track3Actions.setTrue()
+    })
+
+    const renderTrack7 = useMemoizedFn(async () => {
+        const result = await axios.get(`./data/track_7.geojson?_t=${time}`)
+        renderTrackGeo(result)
+        track7Actions.setTrue()
+    })
+
+    const renderTrack14 = useMemoizedFn(async () => {
+        const result = await axios.get(`./data/track_14.geojson?_t=${time}`)
+        renderTrackGeo(result)
+        track14Actions.setTrue()
+    })
+
+    const renderTrackLong = useMemoizedFn(async () => {
+        const result = await axios.get(`./data/track_long.geojson?_t=${time}`)
+        renderTrackGeo(result)
+        trackLongActions.setTrue()
+    })
+
+    useMount(() => {
+        renderRisk()
+        renderTrack3()
+        renderTrack7()
+        renderTrack14()
+        renderTrackLong()
+    })
 
     return (
         <div className='p-2 text-base bg-white rounded-lg pointer-events-auto'>
@@ -38,32 +72,54 @@ const Legend = React.memo(({ riskOver, trackOver }: LegendProps) => {
                 className='flex items-center space-x-1'
                 style={{ color: '#c2410c' }}
             >
-                {trackIcon}
+                {track3 ? (
+                    <ExclamationCircleIcon className='h-4' />
+                ) : (
+                    <DotLoading color='currentColor' />
+                )}
                 <span>{`发布<3天`}</span>
             </div>
             <div
                 className='flex items-center space-x-1'
                 style={{ color: '#f97316' }}
             >
-                {trackIcon}
+                {track7 ? (
+                    <ExclamationCircleIcon className='h-4' />
+                ) : (
+                    <DotLoading color='currentColor' />
+                )}
                 <span>{`发布<7天`}</span>
             </div>
             <div
                 className='flex items-center space-x-1'
                 style={{ color: '#fdba74' }}
             >
-                {trackIcon}
+                {track14 ? (
+                    <ExclamationCircleIcon className='h-4' />
+                ) : (
+                    <DotLoading color='currentColor' />
+                )}
                 <span>{`发布<14天`}</span>
             </div>
             <div
                 className='flex items-center space-x-1'
                 style={{ color: '#ffedd5' }}
             >
-                {trackIcon}
+                {trackLong ? (
+                    <ExclamationCircleIcon className='h-4' />
+                ) : (
+                    <DotLoading color='currentColor' />
+                )}
                 <span>{`发布≥14天`}</span>
             </div>
         </div>
     )
 })
+
+const renderTrackGeo = ({ data }: any) => {
+    Object.values(data).forEach((track) =>
+        window.map.getLayer('track').addGeometry(track)
+    )
+}
 
 export default Legend
