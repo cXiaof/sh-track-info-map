a6cbc79e8ca668fa83f0ea0b3df3d75090a53885
cXiaof <310257509@qq.com>
2022-07-02 16:03:32 +0800

    调整聚合层级

--- src/constants/mapConfig.ts
+++ src/constants/mapConfig.ts
@@ -23,7 +23,7 @@ const map = new maptalks.Map('map', {
 })
 
 new maptalks.TileClusterLayer('track_long', {
-  maxClusterZoom: 16,
+  maxClusterZoom: 17.5,
   clusterDispersion: true,
   markerEvents: {
     click: (e: any) => {
